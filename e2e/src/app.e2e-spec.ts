2f004363d4400df0570c904d9ef5982b7864463b
Santosh mahto <mahtos480@gmail.com>
2019-03-10 08:09:44 +0530

    init

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to iot-onsi!');
+    expect(page.getTitleText()).toEqual('Welcome to iot-onsi-web!');
   });
 
   afterEach(async () => {
